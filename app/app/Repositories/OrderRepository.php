22bfeb3549532defef5e49d2a1d244db6390b5e0
jalil betroji <jalil.betroji@gmail.com>
2024-02-01 16:24:46 +0100

    back-end logic added

--- app/app/Repositories/OrderRepository.php
+++ app/app/Repositories/OrderRepository.php
@@ -2,6 +2,17 @@
 
 namespace App\Repositories;
 
+use App\Models\Order;
+use App\Models\Product;
+use App\Repositories\BaseRepository;
+
 class OrderRepository extends BaseRepository {
-    //
+    public function __construct(Order $order){
+        parent::__construct($order);
+    }
+    
+    public function searchOrder($search){
+        return Order::join('products','orders.productId','=','products.id')->where('nom', 'like', '%' . $search . '%')
+        ->orWhere('city','like','%'. $search . '%')->orWhere('nom','like','%'. $search . '%')->paginate(3);
+    }
 }
